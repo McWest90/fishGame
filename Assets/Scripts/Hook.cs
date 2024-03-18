3201b496e4c210a9ec86107357a6c32f83fc32e2
stas <Mcwest90@gmail.com>
2024-03-18 18:17:26 +0200

    hook logick and  fissh logick

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 public class Hook : MonoBehaviour
 {
@@ -37,5 +38,49 @@ public class Hook : MonoBehaviour
             position.x = vector.x;
             transform.position = position;
         }
+    }
+
+    public void StartFishing()
+    {
+        length = -50;
+        strength = 3;
+        fishCount = 0;
+        float time = (-length) * 0.1f;
+
+        cameraTween = mainCamera.transform.DOMoveY(length, 1 + time * 0.25f, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y <= -11)
+                transform.SetParent(mainCamera.transform);
+        }).OnComplete(delegate
+        {
+            coll.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time * 5, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -25f)
+                    StopFishing();
+            });
+        });
+
+        coll.enabled = false;
+        canMove = true;
+    }
+    
+    void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -11)
+                {
+                    transform.SetParent(null);
+                    transform.position = new Vector2(transform.position.x, -6);
+                }
+            }).OnComplete(delegate
+            {
+                transform.position = Vector2.down * 6;
+                coll.enabled = true;
+                int num = 0;
+            });
     }
 }
